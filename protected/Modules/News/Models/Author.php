3a3ab007d592be88a3601e3558ed92e70779bc16
Oleg19 <oshkinoleg@mail.ru>
2015-01-13 22:36:06 +0300

    Изучение Framework T4

--- protected/Modules/News/Models/Author.php
+++ protected/Modules/News/Models/Author.php
@@ -8,11 +8,7 @@
 
 namespace App\Modules\News\Models;
 
-use T4\Core\Collection;
-use T4\Core\Exception;
-use T4\Fs\Helpers;
-use T4\Http\Uploader;
-use T4\Mvc\Application;
+
 use T4\Orm\Model;
 
 class Author
